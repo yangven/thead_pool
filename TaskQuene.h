8dd575abd5d08c9fb24ffd26beac07018a05495d
yangven <911917634@qq.com>
2026-07-16 12:53:49 +0800

    完成类声明的头文件

---
+++ TaskQuene.h
@@ -0,0 +1,43 @@
+# include <queue>
+# include <pthread.h>
+
+
+using callback = void (*)(void* arg);
+
+//任务结构体
+struct Task {
+    Task()
+    {
+        function = nullptr;
+        arg = nullptr;
+    }
+    Task(callback f, void* arg)
+    {
+        this->arg = arg;
+        function = f;
+    }
+    callback function;
+    //void (*function)(void*arg);
+    void* arg;
+};
+
+
+class TaskQueue {
+public:
+    TaskQueue();
+    ~TaskQueue();
+
+    //添加任务
+    void addTask(Task task);
+    //取出任务
+    void takeTask();
+    //获取当前任务个数
+    inline int taskNumber(){
+        return m_taskQ.size();
+    }
+
+
+private:
+    pthread_mutex_t m_mutex;
+    std::queue<Task> m_taskQ;
+};
